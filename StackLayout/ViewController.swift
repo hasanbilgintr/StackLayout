284265230643e2e47719b87e4cbbd07f21321299
Hasan Bilgin <hasanbilgintr@gmail.com>
2024-01-11 03:13:53 +0300

    first commit

---
+++ StackLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StackLayout
+//
+//  Created by hasan bilgin on 17.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //stack View kullanımı storyboardda seçeceğimiz itemleri seçeriz -> sağ alatta enbed in-> Stack View seçilir yada direk library + ile yazılıp getirtilebilir
+    //gruplaştırma diyebiliriz bu şekilde kendi arasındaki mesafeler daha düzenli yapmış oluruz
+    
+    //Not imageView lere genişlik ve yükselik sabit verilirse resimler değişse bile aynı boyutta kalıcaktır.Verilmezzse boyut neyse büyücektir
+
+
+}
+
